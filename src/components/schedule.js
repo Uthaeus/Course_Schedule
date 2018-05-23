ae2d31ed621a880fe959d9e24b25111170685a67
Uthaeus <romanlavery@gmail.com>
2018-05-23 15:18:00 -0600

    fixed ordering of schedule

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -8,6 +8,10 @@ class Schedule extends Component {
     super(props)
 
     this.renderCourse = this.renderCourse.bind(this);
+
+    this.state = {
+      enrolled: []
+    }
   }
 
   renderCourse(course) {
@@ -19,17 +23,34 @@ class Schedule extends Component {
     )
   }
 
+  componentWillReceiveProps(nextProps) {
+    var newEnrolled = []
+
+    this.state.enrolled.map((course) => {
+      if (course.enrolled) {
+        newEnrolled.push(course);
+      }
+    })
+
+    nextProps.courses.map((course) => {
+      if (course.enrolled && !newEnrolled.includes(course)) {
+        newEnrolled.push(course);
+      }
+    })
+
+    this.setState({
+      enrolled: newEnrolled
+    })
+  }
+
   render() {
     return (
       <div>
         <div className="schedule__slots">
           {
-            //filter out course instances
-
-            //ordering
 
             //empty slots
-            this.props.courses.map(this.renderCourse)
+            this.state.enrolled.map(this.renderCourse)
           }
         </div>
       </div>
